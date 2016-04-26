d30eccad6c14b8915b5ac3b50176c4e9a4c73efc
Andreas Altendorfer <andreas@altendorfer.at>
2016-04-26 21:53:36 +0200

    Add glyphicons for sort button

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -9,6 +9,13 @@ defmodule Iboard.PageView do
     end
   end
 
+  def chevron_up label do
+    {:safe,["<span title='Now displaying oldest first. Click to change' class='glyphicon glyphicon-chevron-up'>","</span>"]}
+  end
+
+  def chevron_down label do
+    {:safe,["<span title='Now displaying latest first. Click to change' class='glyphicon glyphicon-chevron-down'>","</span>"]}
+  end
 
   defp more_link(conn,model) do
     {:safe, parts} = link 'more', to: page_path(conn,:show, model)
